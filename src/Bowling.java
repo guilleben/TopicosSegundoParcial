cf6889a4bb5c2d648b8c20b3f8d4be04ab9bbd11
guilleben <guillermobenasulin@gmail.com>
2017-11-27 17:26:39 -0300

    Test3 ROJO

--- src/Bowling.java
+++ src/Bowling.java
@@ -19,11 +19,22 @@ public class Bowling {
 	@Test
 	public void PeorPartida() {
 		NuevaPartida partida = new NuevaPartida();
-		for (int i = 0; i <= 10; i++) {
+		for (int i = 1; i <= 10; i++) {
 			partida.rodar(0,0);
 			
 		}
 		assertEquals(0, partida.GetPuntuacion());
+		
+	}
+	
+	@Test
+	public void MejorPartida() {
+		NuevaPartida partida = new NuevaPartida();
+		for (int i = 1; i <= 10; i++) {
+			partida.rodar(10,0);
+			
+		}
+		assertEquals(100, partida.GetPuntuacion());
 		
 	}
 
