e72d2311c510027c7895008b5518ba1970a44c8f
guilleben <guillermobenasulin@gmail.com>
2017-11-27 17:37:06 -0300

    Test3 VERDE

--- src/NuevaPartida.java
+++ src/NuevaPartida.java
@@ -1,14 +1,17 @@
 
 public class NuevaPartida {
+	
+	private int puntuacion = 0;
 
 	public void rodar(int i, int j) {
 	
+	 puntuacion += (i+j); 
 		
 	}
 
 	public Object GetPuntuacion() {
 		
-		return 0;
+		return puntuacion;
 	}
 
 }
